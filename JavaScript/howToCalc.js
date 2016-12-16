532a59acb025bba3849622707cd8f60ec43f866b
Alex Truong <atruong.sh@mysummitps.org>
2016-12-16 21:22:06 +0000

    temporary fix index.html

---
+++ JavaScript/howToCalc.js
@@ -0,0 +1,47 @@
+// var Main = document.getElementById(Main);
+
+var html = document.getElementById("html"); //HTML Title
+var htmlImg = document.getElementById("cssImg"); //HTML IMG
+
+var css = document.getElementById("css");  //CSS Title
+var cssImg = document.getElementById("cssImg"); //CSS IMG
+
+var js = document.getElementById("js"); //JavaScript Title
+var jsImg = document.getElementById("jsImg"); //JavaScript IMG
+
+var htmlInfo = document.getElementById("htmlInfo"); 
+//Add more vars
+
+var cssInfo = document.getElementById("cssInfo");
+//Add more vars
+
+var jsInfo = document.getElementById("jsInfo");
+//Add more vars
+ 
+//Title of the languages
+html.addEventListener("click", html);
+css.addEventListener("click", css);
+js.addEventListener("click", js);
+
+//The information for each category
+htmlInfo.addEventListener("click", htmlInfo);
+cssInfo.addEventListener("click", cssInfo);
+jsInfo.addEventListener("click", jsInfo);
+
+function html() {
+   if ("click", #)
+       
+   
+}
+
+function htmlInfo() {
+    
+}
+
+function cssInfo() {
+    
+}
+
+function jsInfo() {
+    
+}
